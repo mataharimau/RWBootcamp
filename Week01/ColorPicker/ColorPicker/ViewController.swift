a926cdbac7bee390ada4766aa4c837928397d368
mataharimau <audrey.tam@gmail.com>
2020-06-15 14:56:25 +1000

    Create new project for Week01 homework

---
+++ Week01/ColorPicker/ColorPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorPicker
+//
+//  Created by Audrey Tam on 15/6/20.
+//  Copyright © 2020 Razeware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
